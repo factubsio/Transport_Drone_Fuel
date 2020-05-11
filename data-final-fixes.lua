e40485626f5f6f207811846d7b1d52384059c0fb
Bubbles <worcesterm@gmail.com>
2020-05-11 07:03:46 +0100

    remove steel from fuel depot recipe

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -8,6 +8,12 @@ end
 
 data.raw.recipe["fuel-depots"].results[1].name = tdf.fuel_name
 data.raw.recipe["fuel-depots"].ingredients[2].name = tdf.fuel_name
+data.raw.recipe["fuel-depot"].ingredients = {
+    {"iron-plate", 100},
+    {"iron-gear-wheel", 30},
+    {"pipe", 20},
+    {"electronic-circuit", 10},
+}
 data.raw.recipe["transport-drone"].ingredients = {
     {"drone-engine-unit", 2},
     {"iron-plate", 5},

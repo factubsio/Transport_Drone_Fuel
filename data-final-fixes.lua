a7231777a30c0c5eb05bc5031bb5c4994927ab38
Bubbles <worcesterm@gmail.com>
2020-05-15 04:46:15 +0100

    if bobs then use bcb not electronic-circuits

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -6,17 +6,23 @@ for name,recipe in pairs(data.raw.recipe) do
     end
 end
 
+local circuit = "electronic-circuit"
+if data.raw.item["basic-circuit-board"] then
+    circuit = "basic-circuit-board"
+end
+
 data.raw.recipe["fuel-depots"].results[1].name = tdf.fuel_name
 data.raw.recipe["fuel-depots"].ingredients[2].name = tdf.fuel_name
 data.raw.recipe["fuel-depot"].ingredients = {
     {"iron-plate", 100},
     {"iron-gear-wheel", 30},
     {"pipe", 20},
-    {"electronic-circuit", 10},
+    {circuit, 10},
 }
+
 data.raw.recipe["transport-drone"].ingredients = {
     {"drone-engine-unit", 2},
     {"iron-plate", 5},
     {"copper-plate", 2},
-    {"electronic-circuit", 4},
+    {circuit, 4},
 }

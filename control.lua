5d738400dae440985c7b4f2478013440bfb8abd6
Bubbles <worcesterm@gmail.com>
2020-06-05 00:40:19 +0100

    make migration from TD a bit better, try to fit in with aai industry, py mods, and bobs electronics a bit better.

--- control.lua
+++ control.lua
@@ -1,15 +1,76 @@
 local events = require("event_handler")
+local util = require("util")
 
 
 events.add_lib{
     on_init = function()
         for _,force in pairs(game.forces) do
             if force.technologies["transport-system"].researched then
-                game.print("Giving transport fuel researches")
+                game.print("TD_fuel: Giving transport fuel researches")
                 force.recipes["transport-drone-refinery"].enabled = true
                 force.recipes["drone-fuel-from-coal"].enabled = true
                 force.recipes["drone-engine-unit"].enabled = true
             end
         end
+
+        local old_fuel = settings.startup["fuel-fluid"].value
+
+
+        local fuel_depot_prototype = game.entity_prototypes["fuel-depot"]
+        local in_pipe_pos = nil
+        if fuel_depot_prototype.fluidbox_prototypes[1].production_type == "input" then
+            in_pipe_pos = fuel_depot_prototype.fluidbox_prototypes[1].pipe_connections[1].positions
+        else
+            in_pipe_pos = fuel_depot_prototype.fluidbox_prototypes[2].pipe_connections[1].positions
+        end
+
+        local dump = game.create_inventory(8)
+
+        for _,surface in pairs(game.surfaces) do
+            local bad_things_count = 0
+            local list = surface.find_entities_filtered{name = {"request-depot", "fuel-depot", "buffer-depot"}}
+            for _,entity in pairs(list) do
+                local amount = entity.remove_fluid{name=old_fuel, amount=1000000000}
+
+
+                if entity.prototype.name == "fuel-depot" then
+                    local search_offset = nil
+                    if entity.direction == defines.direction.north then search_offset = in_pipe_pos[1]
+                    elseif entity.direction == defines.direction.east then search_offset = in_pipe_pos[2]
+                    elseif entity.direction == defines.direction.south then search_offset = in_pipe_pos[3]
+                    elseif entity.direction == defines.direction.west then search_offset = in_pipe_pos[4]
+                    end
+
+                    local search_offset = in_pipe_pos[entity.direction/2 + 1]
+                    local search_pos = util.copy(entity.position)
+                    search_pos.x = search_pos.x + search_offset.x
+                    search_pos.y = search_pos.y + search_offset.y
+                    local entity_to_mine = surface.find_entities_filtered{position = search_pos, radius = 0.1}[1]
+                    if entity_to_mine then
+                        entity_to_mine.mine{inventory = dump, raise_destroyed = false}
+                        local items = dump.get_contents()
+                        for dump_name,dump_amount in pairs(items) do
+                            surface.spill_item_stack(search_pos, {name = dump_name, amount = dump_amount, allow_belts = false})
+                        end
+                        dump.clear()
+                        bad_things_count = bad_things_count + 1
+                    end
+                    -- surface.find_entities_filtered{}
+
+                    entity.set_recipe("fuel-depots-bubbles")
+                    if amount > 0 then
+                        entity.insert_fluid{name = "drone-fuel", amount = amount}
+                    end
+                end
+            end
+            if bad_things_count == 1 then
+                game.print("TD_fuel: A contaminated fluid box connected to fuel depots was mined (on surface: " .. surface.name .. ")")
+            elseif bad_things_count > 1 then
+                game.print("TD_fuel: " .. tostring(bad_things_count) .. " contaminated fluid boxes connected to fuel depots were mined (on surface: " .. surface.name .. ")")
+            end
+        end
+
+        dump.destroy()
+
     end
 }
--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,4 +1,5 @@
 local tdf = require("tdf_shared")
+local util = require("util")
 
 for name,recipe in pairs(data.raw.recipe) do
     if recipe.category == "transport-drone-request" then
@@ -6,13 +7,41 @@ for name,recipe in pairs(data.raw.recipe) do
     end
 end
 
+-- defaults
+local engine_unit = "drone-engine-unit"
 local circuit = "electronic-circuit"
+local circuit_count = 4
+
+-- choose circuit board - bobs gets priority then pyhightech
 if data.raw.item["basic-circuit-board"] then
     circuit = "basic-circuit-board"
+elseif mods["pyhightech"] then
+    -- simple circuit boards are still called 'electronic-circuit'
+    circuit_count = 3
 end
 
+-- engine unit tweaks
+if mods["aai-industry"] then
+    engine_unit = "electric-motor"
+    data.raw.recipe["drone-engine-unit"].hidden = true
+elseif mods["pypetroleumhandling"] then
+    data.raw.recipe["drone-engine-unit"].ingredients = {
+        { name = "small-parts-01", amount = 1},
+        { name = "pipe", amount = 2},
+    }
+end
+
+
 data.raw.recipe["fuel-depots"].results[1].name = tdf.fuel_name
-data.raw.recipe["fuel-depots"].ingredients[2].name = tdf.fuel_name
+
+local new_fuel_recipe = util.copy(data.raw.recipe["fuel-depots"])
+new_fuel_recipe.name = "fuel-depots-bubbles"
+new_fuel_recipe.ingredients[2].name = tdf.fuel_name
+
+data:extend{new_fuel_recipe}
+
+data.raw["assembling-machine"]["fuel-depot"].fixed_recipe = nil
+
 data.raw.recipe["fuel-depot"].ingredients = {
     {"iron-plate", 100},
     {"iron-gear-wheel", 30},
@@ -20,9 +49,10 @@ data.raw.recipe["fuel-depot"].ingredients = {
     {circuit, 10},
 }
 
+
 data.raw.recipe["transport-drone"].ingredients = {
-    {"drone-engine-unit", 2},
+    {engine_unit, 2},
     {"iron-plate", 5},
     {"copper-plate", 2},
-    {circuit, 4},
+    {circuit, circuit_count},
 }
